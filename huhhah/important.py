82b6d5b1fab56f4329888141a8ccca85bc9c8cf2
smausquared <samu.ayhynmaki@gmail.com>
2023-11-06 19:09:26 +0200

    tiedostot ja hakemistot

---
+++ huhhah/important.py
@@ -0,0 +1,12 @@
+# olisipa parempi tapa tehdä tämä :thinking:
+yksi = 1
+kaksi = 2
+kolme = 3
+nelja = 4
+viisi = 5
+kuusi = 6
+seitteman = 7
+kaheksan = 8
+yheksan = 9
+
+print(f"{kaksi} plus {kaksi} on {nelja}, miinus {yksi} se on {kolme} nopeet matikat!")
